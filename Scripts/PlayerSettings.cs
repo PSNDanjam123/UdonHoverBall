9cd76dbd206d166b863bb67eb8dba9d0a3280aca
Jamie Bailey <psndanjam123@gmail.com>
2023-03-29 21:42:06 +0100

    fixed bug

--- Scripts/PlayerSettings.cs
+++ Scripts/PlayerSettings.cs
@@ -80,7 +80,7 @@ public class PlayerSettings : UdonSharpBehaviour
 
     public override void InputUse(bool value, UdonInputEventArgs args)
     {
-        if (Fuel < 0.2 || _playerApi.IsPlayerGrounded())
+        if (!value || Fuel < 0.2 || _playerApi.IsPlayerGrounded() || _usingFuel)
         {
             _usingFuel = false;
             return;
